5a7baba8d09838ed14b7ef25b9f22a6b784e38fa
Edward <eallencrow@gmail.com>
2020-02-21 17:59:59 -0600

    Initial Commit

---
+++ src/main/java/com/github/eallencrow/BookstoreSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.github.eallencrow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoreSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstoreSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/eallencrow/BookstoreSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.eallencrow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
